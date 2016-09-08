d9eadaf3602afb247c65165c46c54116696255b5
bredele <olivier.wietrich@gmail.com>
2016-09-07 22:11:00 -0600

    append promise returning primitive

--- index.js
+++ index.js
@@ -9,7 +9,13 @@
 module.exports = function(el, value) {
   if(typeof value === 'function') value = value()
   if(typeof value === 'object') {
-
+  	if(typeof value.then === 'function') {
+  		var tmp = document.createTextNode('')
+  		value.then(function(data) {
+  			tmp.nodeValue = data
+  		})
+  		value = tmp
+  	}
   } else value = document.createTextNode(value)
   el.appendChild(value)
 }
