7c596760dacce141306341620bc564f87eed29e3
bredele <olivier.wietrich@gmail.com>
2016-09-07 22:03:07 -0600

    append primitives

--- test/regurgitate.js
+++ test/regurgitate.js
@@ -11,3 +11,13 @@ tape('should append a string', test => {
 	regurgitate(el, 'hello')
 	test.equal(el.outerHTML, '<div>hello</div>')
 })
+
+
+tape('should append a primitive', test => {
+	test.plan(1)
+	var el = document.createElement('div')
+	regurgitate(el, 'hello')
+	regurgitate(el, true)
+	regurgitate(el, 10)
+	test.equal(el.outerHTML, '<div>hellotrue10</div>')
+})
